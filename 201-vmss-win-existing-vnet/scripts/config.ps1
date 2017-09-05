59257bfc2175300fb8111fba11fd82894b3af981
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-06 08:51:23 +1000

    commit

--- 201-vmss-win-existing-vnet/scripts/config.ps1
+++ 201-vmss-win-existing-vnet/scripts/config.ps1
@@ -3,4 +3,4 @@ Set-WinSystemLocale en-AU
 Set-WinUserLanguageList -LanguageList en-AU
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
-[Environment]::SetEnvironmentVariable("PLEXOS_TEMP", "My Value", "P:\Temp")
+[Environment]::SetEnvironmentVariable("PLEXOS_TEMP", "P:\Temp", "Machine")
