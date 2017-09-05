d9d65a505e750f0ef8a0a7173be6a8dc9a736026
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-06 08:56:05 +1000

    commit

--- 201-vmss-win-existing-vnet/scripts/config.ps1
+++ 201-vmss-win-existing-vnet/scripts/config.ps1
@@ -4,3 +4,4 @@ Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
 [Environment]::SetEnvironmentVariable("PLEXOS_TEMP", "P:\Temp", "Machine")
+Set-TimeZone -Name "AUS Eastern Standard Time"
