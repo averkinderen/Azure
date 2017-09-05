11f1ba39a6d68f5e4098e149fda7aa8fe67ff968
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-06 09:31:12 +1000

    commit

--- 201-vmss-win-existing-vnet/scripts/config.ps1
+++ 201-vmss-win-existing-vnet/scripts/config.ps1
@@ -1,7 +1,8 @@
-﻿Write-Host "Hello World!"
+﻿Stop-Service "PLEXOS Connect Client Service"
 Set-WinSystemLocale en-AU
 Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
 [Environment]::SetEnvironmentVariable("PLEXOS_TEMP", "P:\Temp", "Machine")
 Set-TimeZone -Name "AUS Eastern Standard Time"
+Start-Service "PLEXOS Connect Client Service"
