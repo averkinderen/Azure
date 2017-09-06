9deba84aa80a6415ae5063992c85e4b4b207091a
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-06 11:00:28 +1000

    commit

--- 201-vmss-win-existing-vnet/scripts/config.ps1
+++ 201-vmss-win-existing-vnet/scripts/config.ps1
@@ -3,9 +3,19 @@ Set-WinSystemLocale en-AU
 Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
+Set-TimeZone -Name "AUS Eastern Standard Time"
 
 $drive = get-volume -FileSystemLabel 'Plexos Data'
 $plexos = $drive.DriveLetter + ':\temp'
 [Environment]::SetEnvironmentVariable("PLEXOS_TEMP", $plexos, "Machine")
-Set-TimeZone -Name "AUS Eastern Standard Time"
+
+
+$machine = hostname
+$name = $machine.Substring($machine.Length - 6)
+$plexoskeyfolder = 'C:\Users\marketsims\AppData\Roaming\PLEXOS'
+$plexosxmlfile = $plexoskeyfolder + '\PLEXOS Connect Client.xml'
+$webclient = New-Object System.Net.WebClient
+$url = "https://raw.githubusercontent.com/averkinderen/Azure/master/201-vmss-win-existing-vnet/xml/" + $name + ".xml"
+$webclient.DownloadFile($url,$plexosxmlfile)
+
 Start-Service "PLEXOS Connect Client Service"
