c84dde6fc5814c80bccc194e858115e1a0abed82
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-06 10:05:56 +1000

    commit

--- 201-vmss-win-existing-vnet/scripts/config.ps1
+++ 201-vmss-win-existing-vnet/scripts/config.ps1
@@ -3,6 +3,9 @@ Set-WinSystemLocale en-AU
 Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
-[Environment]::SetEnvironmentVariable("PLEXOS_TEMP", "P:\Temp", "Machine")
+
+$drive = get-volume -FileSystemLabel 'Plexos Data'
+$plexos = $drive.DriveLetter + ':\temp'
+[Environment]::SetEnvironmentVariable("PLEXOS_TEMP", $plexos, "Machine")
 Set-TimeZone -Name "AUS Eastern Standard Time"
 Start-Service "PLEXOS Connect Client Service"
