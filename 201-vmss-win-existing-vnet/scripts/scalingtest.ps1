41c91a0da9ffb478e8e5bc9bd967aa5bc2ef1c65
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-07 10:27:55 +1000

    scalingtest

---
+++ 201-vmss-win-existing-vnet/scripts/scalingtest.ps1
@@ -0,0 +1,20 @@
+﻿#################################################
+# Author: Alexandre Verkinderen
+# Company: cubesys
+# date: 05/09/2017
+################################################
+
+
+Login-AzureRmAccount
+
+#region variables
+$rgName = "MTPASA"
+$scalesetename = "mtpasass7"
+$vmss = Get-AzureRmVmss -ResourceGroupName $rgName -VMScaleSetName $scalesetename
+$csv = "C:\Dropbox\My Documents\OneDrive - cubesys\AEMO\aemo.csv"
+#endregion
+
+$capacity = import-csv $csv
+
+$vmss.sku.capacity = $capacity.servers
+Update-AzureRmVmss -ResourceGroupName $rgName -Name "scale set name" -VirtualMachineScaleSet $vmss 
