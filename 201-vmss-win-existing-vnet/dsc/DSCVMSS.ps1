66ac96d2863206170b7345728b097943af36fa9b
alexandre verkinderen <alexandre@verkinderen.com>
2017-09-28 09:32:30 +1000

    commit

---
+++ 201-vmss-win-existing-vnet/dsc/DSCVMSS.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -version 4.0 
+Configuration SetTimeZone 
+{ 
+    Param 
+    ( 
+        #Target nodes to apply the configuration 
+        [Parameter(Mandatory = $false)] 
+        [ValidateNotNullorEmpty()] 
+        [String]$SystemTimeZone="Romance Standard Time" 
+    ) 
+
+    Import-DSCResource -ModuleName xTimeZone 
+
+    Node localhost 
+    { 
+        xTimeZone TimeZoneExample 
+        { 
+            TimeZone = $SystemTimeZone
+        } 
+    } 
+}
