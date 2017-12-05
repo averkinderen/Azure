f50902f057407921f32440b7432e0be48672ea70
alexandre verkinderen <alexandre@verkinderen.com>
2017-12-05 11:46:16 +1100

    commit

---
+++ 201-vmss-win-existing-vnet/dsc/DSCconfig.ps1
@@ -0,0 +1,39 @@
+﻿Configuration Main
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullorEmpty()]
+        [PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullorEmpty()]
+        [String]
+        $AccountDescription,
+
+        [Parameter(Mandatory = $false)] 
+        [ValidateNotNullorEmpty()] 
+        [String]$SystemTimeZone="Romance Standard Time" 
+    )
+
+    Import-DSCResource -ModuleName xTimeZone 
+    Node Localhost
+    {
+        User NewUser
+        {
+            UserName             = $Credential.UserName
+            Description          = $AccountDescription
+            Disabled             = $false
+            Ensure               = 'Present'
+            Password             = $Credential.Password
+            PasswordNeverExpires = $true
+        }
+
+        xTimeZone TimeZoneExample 
+        { 
+            TimeZone = $SystemTimeZone
+        } 
+
+    }
+}
