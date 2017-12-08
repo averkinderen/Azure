ef468a7858dd95eef09b81b314526621ff2189c4
alexandre verkinderen <alexandre@verkinderen.com>
2017-12-08 16:40:54 +1100

    commit

--- 201-vmss-win-existing-vnet/dsc/DSCconfig.ps1
+++ 201-vmss-win-existing-vnet/dsc/DSCconfig.ps1
@@ -15,12 +15,21 @@
 
     Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
     Node $nodeName
+    PsDscAllowPlainTextPassword = $true
     {
+            User NewUser
+        {
+            UserName             = $Credential.UserName
+            Disabled             = $false
+            Ensure               = 'Present'
+            Password             = $Credential.Password
+            PasswordNeverExpires = $true
+        }
 
                 xEnvironment CreatePathEnvironmentVariable
         {
-            Name = 'TestPathEnvironmentVariable'
-            Value = 'TestValue'
+            Name = 'PLEXOS_TEMP'
+            Value = 'F:\Temp'
             Ensure = 'Present'
             Path = $true
             Target = @('Process', 'Machine')
