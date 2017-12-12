3ef6b93eec6a3416d60129eab276414e0212cb46
alexandre verkinderen <alexandre@verkinderen.com>
2017-12-12 15:50:56 +1100

    commit

--- 201-vmss-win-existing-vnet/dsc/DSCconfig.ps1
+++ 201-vmss-win-existing-vnet/dsc/DSCconfig.ps1
@@ -119,15 +119,17 @@
             DependsOn = "[File]PlexosFolder"
         }
 
-        #Package ConnectClient
-       # {
-        #Ensure      = "Present"
-        #Path        = "$env:SystemDrive\PLEXOS.Connect.Client.3.00R01.msi"
-        #Name        = "PLEXOS Connect Client"
-        #ProductId   = "6603B46E-B758-4255-9006-3CB4F273672D"
-        #DependsOn = "[xRemoteFile]ConnectMSI"
-        #} 
-
+        Package ConnectClient
+        {
+        Ensure      = "Present"
+        Path        = "$env:SystemDrive\PLEXOS.Connect.Client.3.00R01.msi"
+        Name        = "PLEXOS Connect Client"
+        ProductId   = "6603B46E-B758-4255-9006-3CB4F273672D"
+        Arguments = "SVCUSER=vmAdmin SVCPASS=P@ssw0rd!1234"
+        LogPath = "F:\Temp\Installplexos.log"
+        DependsOn = "[xRemoteFile]ConnectMSI"
+        } 
+        
         Language ConfigureLanguage 
         {
             IsSingleInstance = "Yes" 
