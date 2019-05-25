df84a8a6cff6df428e4a76051483ebe95224060b
Alexandre Verkinderen <alexandre@verkinderen.com>
2019-05-25 13:30:47 +1000

    added blogpost link

--- 101-ServerBuild/serverbuild.ps1
+++ 101-ServerBuild/serverbuild.ps1
@@ -1,6 +1,6 @@
 # Script to define regional settings on Azure Virtual Machines deployed from the market place
-#
-# Blogpost: 
+# Author: Alexandre Verkinderen
+# Blogpost: https://mscloud.be/configure-regional-settings-and-windows-locales-on-azure-virtual-machines/
 #
 ######################################33
 
@@ -17,13 +17,13 @@ $webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 # Set Locale, language etc. 
 & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
 
-# Set languages/culture
+# Set languages/culture. Not needed perse.
 Set-WinSystemLocale en-AU
 Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
 Set-TimeZone -Name "E. Australia Standard Time"
 
-# restart virtual machine to apply regional settings to current user.
+# restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
 Start-sleep -Seconds 40
 Restart-Computer
