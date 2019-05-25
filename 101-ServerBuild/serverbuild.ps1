0d4fbe52fab5edd9e103b50f355d419b8ca20d0e
Alexandre Verkinderen <alexandre@verkinderen.com>
2019-05-25 10:43:51 +1000

    Added comments

--- 101-ServerBuild/serverbuild.ps1
+++ 101-ServerBuild/serverbuild.ps1
@@ -1,6 +1,15 @@
+# Script to define regional settings on Azure Virtual Machines deployed from the market place
+#
+# Blogpost: 
+#
+######################################33
+
+#variables
 $regionalsettingsURL = "https://raw.githubusercontent.com/averkinderen/Azure/master/101-ServerBuild/AURegion.xml"
 $RegionalSettings = "D:\AURegion.xml"
-#downdload files
+
+
+#downdload regional settings file
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 
@@ -14,3 +23,7 @@ Set-WinUserLanguageList -LanguageList en-AU -Force
 Set-Culture -CultureInfo en-AU
 Set-WinHomeLocation -GeoId 12
 Set-TimeZone -Name "E. Australia Standard Time"
+
+# restart virtual machine to apply regional settings to current user.
+Start-sleep -Seconds 40
+Restart-Computer
